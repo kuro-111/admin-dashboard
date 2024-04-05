3a620d9df4b9e282dfaa6b3d8a51d2344dc4962b
graceann <graceann@XUbuntu.myguest.virtualbox.org>
2024-04-05 10:14:15 +0800

    Add a dad jokes API on search input

---
+++ script.js
@@ -0,0 +1,35 @@
+const input = document.querySelector('input');
+input.addEventListener("focusin", ()=> {
+    dadJokes = false;
+    input.placeholder = " ";
+    return
+});
+input.addEventListener("focusout", ()=> {
+    dadJokes = true;
+});
+
+
+let dadJokes = true ;
+
+
+function dadjokesplaceHolder (event) {
+    if(dadJokes) {
+        axios.get("https://api.api-ninjas.com/v1/dadjokes?limit=1", {
+            headers: {
+                "X-Api-Key": "G4aAC+vXSIrdIy1N/+8E1A==ACJRTMkqp3z3ZUnu"
+            }
+        }).then((res)=> {
+            input.placeholder = res.data[0].joke;
+            console.log(res.data[0].joke);
+        }).catch((err)=> {
+            console.log(err.message);
+            return;
+        })
+        
+    }
+}
+
+setInterval(()=> {
+    dadjokesplaceHolder();
+}, 7000)
+
